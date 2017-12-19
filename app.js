d953461704ca85f4242d91397649bf6711ce652f
ecoSync <ecosync@Apple-Macintosh.local>
2017-12-19 14:50:06 +0700

    Add 1st route

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+//1st route 
+app.get('/', (req, res) => {
+    console.log('Hello Happy :)');
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
