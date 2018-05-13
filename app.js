c80f143786284a59dfe10ae21a72463713795450
ecosyn <psinthorn@gmail.com>
2018-05-13 23:27:49 +0700

    fixed hoek security

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ mongoose.Promise = global.Promise;
 
 //MongoDB connect
 mongoose.connect(keys.mongoURI, {
-   useMongoClient: true
+//    useMongoClient: true
 })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err) );
