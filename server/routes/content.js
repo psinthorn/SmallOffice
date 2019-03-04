e859cf8ae270f6d9c09c9a4c458094b1caa20793
Sinthorn Pradutnam <psinthorn@gmail.com>
2019-03-04 09:24:07 +0700

    Sub header update

--- server/routes/content.js
+++ server/routes/content.js
@@ -1,114 +1,97 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const User = mongoose.model('user');
-const Content = mongoose.model('content');
-const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
+const mongoose = require("mongoose");
+const User = mongoose.model("user");
+const Content = mongoose.model("content");
+const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
 
-
-
-router.get('/', (req, res) => {
-    
-        Content.find()
-        .populate('user')
-        .then(contents => {
-            res.render('contents/index', {
-                contents: contents
-            });  
-        });
-        
-        
-
+router.get("/", (req, res) => {
+  Content.find()
+    .populate("user")
+    .then(contents => {
+      res.render("contents/index", {
+        contents: contents
+      });
+    });
 });
 
-router.get('/add', ensureAuthenticated, (req, res) => {
-    
-        res.render('contents/add');
-        
-
+router.get("/add", ensureAuthenticated, (req, res) => {
+  res.render("contents/add");
 });
 
+//add Content form post
+router.post("/", ensureAuthenticated, (req, res) => {
+  // res.send('post content');
+  let allowComments;
+  let errors = [];
+  let successMsg = [];
 
-    //add Content form post
-router.post('/', ensureAuthenticated, (req, res) => {
-    
-    // res.send('post content');
-        let allowComments;
-        let errors = [];
-        let successMsg = [];
+  if (req.body.allowComments) {
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
 
-        if (req.body.allowComments) {
-            allowComments = true;
-        } else {
-            allowComments = false;
-        }
-    
-        let user = req.user.id;
-        if(user == undefined || user == '' || user == null){
-            res.render('auth/google');
-        }
-    
-        if (!req.body.title) {
-            errors.push({ error: '* Content title is require' })
-        }
-    
-        if (!req.body.body) {
-            errors.push({ error: '* Content body is require' })
-        }
-    
-        if (!req.body.status) {
-            errors.push({ error: '* Status is require' })
-        }
-    
-        if (errors.length > 0) {
-            res.render('contents/add', {
-                errors: errors,
-                title: req.body.title,
-                body: req.body.body,
-                status: req.body.status,
-                allowComments: allowComments,
-            });
-    
-        } else {
-    
-            successMsg.push({ success: `Your new content is addded, Click dashboard to view your contents ` });
-    
-            if (req.body.allowComments) {
-                allowComments = true;
-            } else {
-                allowComments = false;
-            }
-    
-            const newContent = {
-                title: req.body.title,
-                body: req.body.body,
-                status: req.body.status,
-                allowComments: allowComments,
-                user: req.user.id
-            }
-    
-            new Content(newContent)
-                .save()
-                .then(content => {
-    
-                    res.render('contents/add', {successMsg: successMsg});
-    
-                });
-        }
+  let user = req.user.id;
+  if (user == undefined || user == "" || user == null) {
+    res.render("auth/google");
+  }
+
+  if (!req.body.title) {
+    errors.push({ error: "* Content title is require" });
+  }
+
+  if (!req.body.body) {
+    errors.push({ error: "* Content body is require" });
+  }
+
+  if (!req.body.status) {
+    errors.push({ error: "* Status is require" });
+  }
+
+  if (errors.length > 0) {
+    res.render("contents/add", {
+      errors: errors,
+      title: req.body.title,
+      body: req.body.body,
+      status: req.body.status,
+      allowComments: allowComments
     });
-    
-    //edit content form
-    router.get('/edit/:id', (req, res) => {
-        Content.findOne({
-            _id: req.params.id
-        })
-            .then(content => {
-                res.render('contents/edit', { content: content });
-            })
-    
+  } else {
+    successMsg.push({
+      success: `Your new content is addded, Click dashboard to view your contents `
     });
 
-    //edit content form
+    if (req.body.allowComments) {
+      allowComments = true;
+    } else {
+      allowComments = false;
+    }
+
+    const newContent = {
+      title: req.body.title,
+      body: req.body.body,
+      status: req.body.status,
+      allowComments: allowComments,
+      user: req.user.id
+    };
+
+    new Content(newContent).save().then(content => {
+      res.render("contents/add", { successMsg: successMsg });
+    });
+  }
+});
+
+//edit content form
+router.get("/edit/:id", (req, res) => {
+  Content.findOne({
+    _id: req.params.id
+  }).then(content => {
+    res.render("contents/edit", { content: content });
+  });
+});
+
+//edit content form
 // router.get('/edit/:id', (req, res) => {
 //     Story.findOne({
 //         _id: req.params.id
@@ -119,60 +102,46 @@ router.post('/', ensureAuthenticated, (req, res) => {
 
 // });
 
-
 //edit contents process
-router.put('/:id', ensureAuthenticated, (req, res) => {
+router.put("/:id", ensureAuthenticated, (req, res) => {
+  let successMsg = [];
 
-    let successMsg = [];
-    
+  Content.findOne({
+    _id: req.params.id
+  }).then(content => {
+    let allowComments;
 
-    Content.findOne({
-        _id: req.params.id
-    })
-        .then(content => {
+    if (req.body.allowComments) {
+      allowComments = true;
+    } else {
+      allowComments = false;
+    }
 
-            let allowComments;
+    //new value
 
-            if (req.body.allowComments) {
-                allowComments = true;
-            } else {
-                allowComments = false;
-            }
+    (content.title = req.body.title),
+      (content.status = req.body.status),
+      (content.allowComments = allowComments),
+      (content.body = req.body.body);
 
-            //new value 
-
-                content.title = req.body.title,
-                content.status = req.body.status,
-                content.allowComments = allowComments,
-                content.body = req.body.body
-
-            //update content
-            content.save()
-                .then(content => {
-                    successMsg.push({success: 'Update success'});
-                    res.render('contents', {successMsg: successMsg});
-                });
-
-        });
-
+    //update content
+    content.save().then(content => {
+      successMsg.push({ success: "Update success" });
+      res.render("contents", { successMsg: successMsg });
+    });
+  });
 });
-
 
 //delete content
-router.delete('/:id', ensureAuthenticated, (req, res) => {
+router.delete("/:id", ensureAuthenticated, (req, res) => {
+  //console.log(req.params.id);
 
-    //console.log(req.params.id);
+  let successMsg = [];
+  successMsg.push({ success: "Content deleted" });
 
-    let successMsg = [];
-    successMsg.push({success: 'Content deleted'});
-
-    Content.remove({ _id: req.params.id })
-        .then(() => {
-            res.render('contents/index', {successMsg: successMsg});
-        });
-
+  Content.remove({ _id: req.params.id }).then(() => {
+    res.render("contents/index", { successMsg: successMsg });
+  });
 });
-    
-
 
 module.exports = router;
--- server/routes/productCategory.js
+++ server/routes/productCategory.js
@@ -1,26 +1,50 @@
+const ProductCategoryController = require("./../controllers/admin/productCategoryController");
+const { ensureAuthenticated, ensureGuest } = require("./../helpers/auth");
 
-const ProductCategoryController = require('./../controllers/admin/productCategoryController');
-const {ensureAuthenticated, ensureGuest} = require('./../helpers/auth');
+module.exports = app => {
+  //888888888888888888888888888
+  //88   Tour Category 88
+  //888888888888888888888888888
 
-module.exports = (app) => {
+  app.get(
+    "/admin/product-category",
+    ensureAuthenticated,
+    ProductCategoryController.getAll
+  );
 
-    //888888888888888888888888888
-    //88   Tour Category 88
-    //888888888888888888888888888
+  app.get(
+    "/admin/product-category/add",
+    ensureAuthenticated,
+    ProductCategoryController.addForm
+  );
 
-    app.get('/admin/tour-category', ensureAuthenticated, ProductCategoryController.getAll ) 
+  app.get(
+    "/admin/product-category/edit/:id",
+    ensureAuthenticated,
+    ProductCategoryController.editForm
+  );
 
-    app.get('/admin/tour-category/add', ensureAuthenticated, ProductCategoryController.addForm);
+  app.post(
+    "/admin/product-category",
+    ensureAuthenticated,
+    ProductCategoryController.create
+  );
 
-    app.get('/admin/tour-category/edit/:id', ensureAuthenticated, ProductCategoryController.editForm)
+  app.put(
+    "/admin/product-category/:id",
+    ensureAuthenticated,
+    ProductCategoryController.editUpdate
+  );
 
-    app.post('/admin/tour-category', ensureAuthenticated, ProductCategoryController.create);
+  app.put(
+    "/admin/product-category/image/:id",
+    ensureAuthenticated,
+    ProductCategoryController.imageUpdate
+  );
 
-    app.put('/admin/tour-category/:id', ensureAuthenticated, ProductCategoryController.editUpdate);
-
-    app.put('/admin/tour-category/image/:id', ensureAuthenticated, ProductCategoryController.imageUpdate);
-
-    app.delete('/admin/tour-category/:id', ensureAuthenticated, ProductCategoryController.delete);
-
-
+  app.delete(
+    "/admin/product-category/:id",
+    ensureAuthenticated,
+    ProductCategoryController.delete
+  );
 };
