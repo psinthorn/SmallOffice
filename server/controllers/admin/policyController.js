72531b7054a9c86b111d35a2c4886fb8beb4834d
Sinthorn Pradutnam <psinthorn@gmail.com>
2019-03-15 17:10:01 +0700

    Policy and About CRUD function update

--- server/controllers/admin/policyController.js
+++ server/controllers/admin/policyController.js
@@ -1,67 +1,67 @@
-const mongoose = require('mongoose');
-const Policy =require('./../../models/Policy');
-
+const mongoose = require("mongoose");
+const Policy = require("./../../models/Policy");
 
 module.exports = {
- 
- 
- //Intro Route
- getPolicy(req, res){
-    Policy.find({ })
-        .then( policy => {
-            res.render('admin/policy', { policy: policy });
-        });
-},
+  //
+  // Policy Route
+  //
 
-policyForm(req, res){
-    res.render('admin/policy-add');
-},
+  // Get all policies
+  getPolicy(req, res) {
+    Policy.find({}).then(policy => {
+      res.render("admin/policy", {
+        policy: policy,
+        sectionTitle: "เงื่อนไข"
+      });
+    });
+  },
 
-addPolicy(req, res){
+  policyForm(req, res) {
+    res.render("admin/policy-add", {
+      sectionTitle: "เพิ่มเงื่อนไข"
+    });
+  },
+
+  addPolicy(req, res) {
     const policyProps = req.body;
     //res.send(policyProps);
-    
-    Policy.create(policyProps)
-        .then(() => Policy.find({}))
-            .then( policy => { 
-                res.render('admin/policy', { policy: policy });
-            });
-},
 
-editPolicyForm(req, res){
-    
+    Policy.create(policyProps)
+      .then(() => Policy.find({}))
+      .then(policy => {
+        res.redirect("/admin/policy");
+      });
+  },
+
+  editPolicyForm(req, res) {
     const id = req.params.id;
 
-    Policy.findById({ _id: id })
-        .then( policy => {
-            res.render('admin/policy-edit', { policy: policy });
-        });
-},
+    Policy.findById({ _id: id }).then(policy => {
+      res.render("admin/policy-edit", {
+        policy: policy,
+        sectionTitle: "แก้ไขเงื่อนไข"
+      });
+    });
+  },
 
-editPolicy(req, res){
-
+  editPolicy(req, res) {
     const policyProps = req.body;
     const id = req.params.id;
-    
-     Policy.findByIdAndUpdate({_id: id }, policyProps)
-        .then(() => Policy.findById({ _id: id }))
-            .then( policy => { 
-                res.render('admin/policy-edit', { policy: policy });
-            });
 
+    Policy.findByIdAndUpdate({ _id: id }, policyProps)
+      .then(() => Policy.findById({ _id: id }))
+      .then(policy => {
+        res.render("admin/policy-edit", { policy: policy });
+      });
+  },
 
-}, 
-
-delete(req, res){
-
+  delete(req, res) {
     const id = req.params.id;
 
     Policy.findByIdAndRemove({ _id: id })
-    .then(() => Policy.find({}))
-        .then( policy => {
-            res.render('admin/policy', { policy: policy });
-        });
-
-}
-
-}
+      .then(() => Policy.find({}))
+      .then(policy => {
+        res.redirect("/admin/policy", { policy: policy });
+      });
+  }
+};
